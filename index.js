dcce6394839f3b8fdee4dd6bb7487ee76b798255
captainWaheed <thecaptain0078@gmail.com>
2023-07-01 16:30:21 +0500

    ✨ feat: Bot working + context chat 🌟

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
-import { Configuration, OpenAIApi } from "openai";
-import dotenv from "dotenv";
-dotenv.config();
+import openai from './config/open-ai.js';
+import readlineSync from 'readline-sync';
+import colors from 'colors';
 
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-})
+async function main() {
+  console.log(colors.bold.green('Welcome to the Chatbot Program!'));
+  console.log(colors.bold.green('You can start chatting with the bot.'));
 
-const openai = new OpenAIApi(configuration);
+  const chatHistory = []; // Store conversation history
 
-async function main(){
+  while (true) {
+    const userInput = readlineSync.question(colors.yellow('You: '));
 
+    try {
+      // Construct messages by iterating over the history
+      const messages = chatHistory.map(([role, content]) => ({
+        role,
+        content,
+      }));
+
+      // Add latest user input
+      messages.push({ role: 'user', content: userInput });
+
+      // Call the API with user input & history
+      const completion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: messages,
+      });
+
+      // Get completion text/content
+      const completionText = completion.data.choices[0].message.content;
+
+      if (userInput.toLowerCase() === 'exit') {
+        console.log(colors.green('Bot: ') + completionText);
+        return;
+      }
+
+      console.log(colors.green('Bot: ') + completionText);
+
+      // Update history with user input and assistant response
+      chatHistory.push(['user', userInput]);
+      chatHistory.push(['assistant', completionText]);
+    } catch (error) {
+      console.error(colors.red(error));
+    }
+  }
 }
 
 main();
